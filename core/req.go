c996b6f9d96eacec33ed60933215ed1f425123dd
0xc4t <71647887+rodnt@users.noreply.github.com>
2021-04-29 13:03:46 -0300

    Update req.go

--- core/req.go
+++ core/req.go
@@ -16,7 +16,7 @@ import (
 	"time"
 
 	"github.com/fatih/color"
-	"github.com/pownx/quickpress/utils"
+	"github.com/rodnt/quickpress/utils"
 )
 
 const ua = "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:74.0) Gecko/20100101 Firefox/74.0 - github.com/pownx)"
